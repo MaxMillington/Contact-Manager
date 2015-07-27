970f384649408d08327e6df3ff0363e9ebae62f7
MaxMillington <maxmillington89@gmail.com>
2015-07-27 08:24:09 -0600

    fixed email address model spec to reflect changes from person to contact

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -2,5 +2,5 @@ class EmailAddress < ActiveRecord::Base
   belongs_to :contact, polymorphic: true
 
   validates :address, presence: true
-  validates :person_id, presence: true
+  validates :contact_id, presence: true
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  let(:email_address) { EmailAddress.new(address: 'money@money.com', person_id: 1) }
+  let(:person) { Person.create(:first_name => "Pat", :last_name => "Metheny") }
+  let(:email_address) { EmailAddress.new(address: 'money@money.com', contact_id: person.id, contact_type: "Person") }
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -12,13 +13,13 @@ RSpec.describe EmailAddress, type: :model do
     expect(email_address).to_not be_valid
   end
 
-  it 'must have reference to a person' do
-    email_address.person_id = nil
+  it 'must have reference to a contact' do
+    email_address.contact_id = nil
     expect(email_address).not_to be_valid
   end
 
-  it 'is associated with a person' do
-    expect(email_address).to respond_to(:person)
+  it 'is associated with a contact' do
+    expect(email_address).to respond_to(:contact)
   end
 
 end
